f4a97bbe1526d48b62cb75345ff87b46abf9de2a
Chris De Groef <chrisdegroef@Chriss-MacBook-Air.local>
2021-04-30 14:26:59 +0200

    JS : exercice nom_prenom UpperCase

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+1.
+// let prenom = prompt("quel est ton nom");
+// let bienvenue = "Bonjour ";
+// alert(bienvenue + prenom);
+2.
+// let age = prompt("quel est ton age");
+// alert(age);
+3.
+// let prenom_nom = prompt("quel est ton prénom et ton nom ?");
+// alert(prenom_nom);
+4.
+let prenom = prompt("quel est ton prénom ?");
+let nom = prompt("quel est ton nom ?");
+
+alert("bonjour " + nom.toUpperCase() + " " + prenom);
